496bd07f5a5f056d4130ead3d75b91106f52ce52
Camden <camdenorrb@me.com>
2017-04-13 06:11:20 -0500

    Moved back the calcFib method.

--- src/main/kotlin/me/camdenorrb/midtermtest/struct/FibonacciIndex.kt
+++ src/main/kotlin/me/camdenorrb/midtermtest/struct/FibonacciIndex.kt
@@ -1,7 +1,5 @@
 package me.camdenorrb.midtermtest.struct
 
-import me.camdenorrb.midtermtest.extension.calcFib
-
 /**
  * Created by camdenorrb on 4/12/17.
  */
@@ -10,8 +8,23 @@ import me.camdenorrb.midtermtest.extension.calcFib
 data class FibonacciIndex(val index: Int = 0) {
 
 	// Calculates the index in the Fibonacci sequence
-	fun calcFib() = index.calcFib()
+	fun calcFib() = calcFib(index)
 
 	// private fun calcFib(index: Int): Int = if (index <= 1) index else calcFib(index - 1) + calcFib(index - 2)
+
+	// Performance favorable way to calculate the nth number in the Fibonacci sequence.
+	fun calcFib(index: Int): Long {
+
+		var returned = 0L
+		var cache = 1L
+
+		for (i in 2..index) {
+			val temp = returned
+			returned += cache
+			cache = temp
+		}
+
+		return returned
+	}
 
 }
