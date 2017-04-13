57a8144899067d01fe2c8ba1f0426c8d984e4b13
Camden <camdenorrb@me.com>
2017-04-13 06:15:10 -0500

    Removed redundancy

--- src/main/kotlin/me/camdenorrb/midtermtest/struct/FibonacciIndex.kt
+++ src/main/kotlin/me/camdenorrb/midtermtest/struct/FibonacciIndex.kt
@@ -7,13 +7,8 @@ package me.camdenorrb.midtermtest.struct
 // A class to hold the index.
 data class FibonacciIndex(val index: Int = 0) {
 
-	// Calculates the index in the Fibonacci sequence
-	fun calcFib() = calcFib(index)
-
-	// private fun calcFib(index: Int): Int = if (index <= 1) index else calcFib(index - 1) + calcFib(index - 2)
-
 	// Performance favorable way to calculate the nth number in the Fibonacci sequence.
-	fun calcFib(index: Int): Long {
+	fun calcFib(): Long {
 
 		var returned = 0L
 		var cache = 1L
@@ -26,5 +21,7 @@ data class FibonacciIndex(val index: Int = 0) {
 
 		return returned
 	}
+
+	// private fun calcFib(index: Int): Int = if (index <= 1) index else calcFib(index - 1) + calcFib(index - 2)
 
 }
